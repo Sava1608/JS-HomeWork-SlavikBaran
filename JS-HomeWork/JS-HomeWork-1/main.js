a420cb78a201c38a38537dd47f49e112699f3022
Sava1608 <vtoper43@gmail.com>
2024-02-22 16:30:48 +0200

    Commit

--- JS-HomeWork/JS-HomeWork-1/main.js
+++ JS-HomeWork/JS-HomeWork-1/main.js
@@ -46,8 +46,8 @@ console.log(person);
 // Третє завдання з домашки -----------------------------------------------------------------------------------------------------
 
 let a=100;
-console.log(a);
+console.log(typeof a);
 let b='100';
-console.log(b);
+console.log(typeof b);
 let c=true;
-console.log(c);
+console.log(typeof c);

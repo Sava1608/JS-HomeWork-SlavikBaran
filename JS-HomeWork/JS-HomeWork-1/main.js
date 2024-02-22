8f6c625648f1ed370b594748a4060cfb3aac73ee
Sava1608 <vtoper43@gmail.com>
2024-02-22 16:08:34 +0200

    Commit

--- JS-HomeWork/JS-HomeWork-1/main.js
+++ JS-HomeWork/JS-HomeWork-1/main.js
@@ -1,53 +1,53 @@
 // Перше завдання з домашки -----------------------------------------------------------------------------------------------------
-let string1= "Hello"
-console.log(string1)
-let string2= "Owu"
-console.log(string2)
-let string3= "com"
-console.log(string3)
-let string4= "ua"
-console.log(string4)
+let string1= "Hello";
+console.log(string1);
+let string2= "Owu";
+console.log(string2);
+let string3= "com";
+console.log(string3);
+let string4= "ua";
+console.log(string4);
 let fullstring = string1 + ' ' + string2 +' ' + string3 + ' ' + string4;
-console.log(fullstring)
+console.log(fullstring);
 // Заюзав два варінти не знав який правильний тому зробив два :D
 let fullstring2=`${string1} ${string2} ${string3} ${string4}`;
-console.log(fullstring2)
+console.log(fullstring2);
 
-let number1= 1
-console.log(number1)
-let number2= 10
-console.log(number2)
-let number3= -999
-console.log(number3)
-let number4= 123
-console.log(number4)
+let number1= 1;
+console.log(number1);
+let number2= 10;
+console.log(number2);
+let number3= -999;
+console.log(number3);
+let number4= 123;
+console.log(number4);
 
-const constanta=3.14  //В завдані підозрювалось щоб зробити константу, чи ні? Ну я подумав що так, як ви можите бачити
-console.log(constanta)
+const constanta=3.14;  //В завдані підозрювалось щоб зробити константу, чи ні? Ну я подумав що так, як ви можите бачити
+console.log(constanta);
 
-let number5= 2.7
-console.log(number5)
-let number6= 16
-console.log(number6)
+let number5= 2.7;
+console.log(number5);
+let number6= 16;
+console.log(number6);
 
-let bool1= true
-console.log(bool1)
-let bool2= false
-console.log(bool2)
+let bool1= true;
+console.log(bool1);
+let bool2= false;
+console.log(bool2);
 
 // Друге завдання з домашки -----------------------------------------------------------------------------------------------------
 
-let firstName ='Baran'
-let middleName='Yaroslav'
-let lastName='Orestovich'
-let person=`${firstName} ${middleName} ${lastName}`
-console.log(person)
+let firstName ='Baran';
+let middleName='Yaroslav';
+let lastName='Orestovich';
+let person=`${firstName} ${middleName} ${lastName}`;
+console.log(person);
 
 // Третє завдання з домашки -----------------------------------------------------------------------------------------------------
 
-let a=100
-console.log(a)
-let b='100'
-console.log(b)
-let c=true
-console.log(c)
+let a=100;
+console.log(a);
+let b='100';
+console.log(b);
+let c=true;
+console.log(c);
